d2709e59eac4b1b1b3dd05c7555503b9c3ff83ad
Randal <snoopisarmy@gmail.com>
2021-02-18 23:25:42 -0500

    3.11 Importing npm Modules

--- Section 3 - Notes App/app.js
+++ Section 3 - Notes App/app.js
@@ -1,10 +1,7 @@
+const validator = require('validator')
 const getNotes = require('./notes.js')
 
 const msg = getNotes ()
-
 console.log(msg)
 
-// const add  = require('./utils.js')
-
-// const sum = add (4, -2)
-// console.log(sum)
+console.log(validator.isURL('https://mead.io'))
